d9c549441884e71d4bc55d58b65967e03f554839
Tom Selfin <selfint@gmail.com>
2020-10-27 22:23:33 +0200

    implement mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,64 @@
+use angular_units::Deg;
+use num::complex::{Complex32, Complex64};
 use piston_window::*;
+use prisma::{Color, FromColor, Hsl, Rgb};
+
+fn steps_in_mandelbrot(n: &Complex32, iterations: u64) -> u64 {
+    let mut temp = n.clone();
+    for iteration in 0..iterations {
+        temp = temp.powu(2) + n;
+        if temp.norm() > 2.0 {
+            return iteration + 1;
+        }
+    }
+
+    0
+}
+
 
 fn main() {
     let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
+        WindowSettings::new("Hello Piston!", [400, 400])
             .exit_on_esc(true).build().unwrap();
     while let Some(event) = window.next() {
         window.draw_2d(&event, |context, graphics, _device| {
-            clear([1.0; 4], graphics);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      context.transform,
-                      graphics);
+            const BLACK: [f32; 4] = [0.0; 4];
+            clear(BLACK, graphics);
+            for real in -200..200 {
+                let real = real as f32;
+                for imaginary in -200..200 {
+                    let imaginary = imaginary as f32;
+                    draw_pixel(
+                        real.clone(),
+                        imaginary.clone(),
+                        128,
+                        context,
+                        graphics
+                    );
+                }
+            }
         });
     }
+}
+
+fn draw_pixel(real: f32, imaginary: f32, iterations: u64, context: Context, graphics: &mut G2d) {
+    let steps = steps_in_mandelbrot(
+        &Complex32::new(
+            real.clone() / 100.0,
+            imaginary.clone() / 100.0
+        ),
+        100,
+    );
+    let hsl = Hsl::new(
+        Deg(360.0 * ((steps as f32) / (iterations as f32)) % 360.0),
+        0.8,
+        0.5,
+    );
+    let (r, g, b) = Rgb::from_color(&hsl).to_tuple();
+    let mut color = [r as f32, g as f32, b as f32, 1.0];
+    let rect: [f64; 4] = [real as f64 + 200.0, imaginary as f64 + 200.0, 1.0, 1.0];
+    if steps == 0 {
+        color = [0.0; 4];
+    }
+    rectangle(color, rect, context.transform, graphics);
 }
